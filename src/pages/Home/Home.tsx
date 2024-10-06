847dbaaf114edc6eeb111298d6b0da4555391f91
MinhTamNT <minhtam78945@gmail.com>
2024-10-06 11:17:33 +0700

    chore : fix ui home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -25,7 +25,6 @@ export const Home: React.FC = () => {
   const [updateLastAccess] = useMutation(UPDATE_LASTETS_ACCESS);
   const client = useApolloClient();
   const dispatch = useDispatch();
-  console.log(user);
   const navigate = useNavigate();
 
   if (loading) return <p>Loading...</p>;
@@ -44,24 +43,20 @@ export const Home: React.FC = () => {
 
   const handleEdit = async (idProject: string) => {
     try {
-      // Update last access time
       await updateLastAccess({
         variables: { projectId: idProject },
       });
 
-      // Fetch members for the project using the project ID
       const { data: membersProject } = await client.query({
         query: GET_MEMEBER_IN_PROJECT,
         variables: { projectId: idProject },
       });
 
-      console.log(membersProject);
       const members = membersProject?.getMememberInProject || [];
       const currentUserRole = members.find(
         (member: any) => member.User[0].idUser === user?.sub
       );
 
-      console.log(currentUserRole);
       if (currentUserRole) {
         dispatch(
           fetchUserRoleSuccess({
@@ -91,59 +86,57 @@ export const Home: React.FC = () => {
           autoPlay
           loop
           muted
-          className="absolute inset-0 w-full h-full object-cover"
+          className="absolute inset-0 w-full h-full object-cover brightness-75"
           src="https://cdn.prod.website-files.com/62bac7754ea6d7967db80305/65d6f41364044a20584b6dea_CZ_Main_Compress-transcode.mp4"
         />
-        <div className="absolute inset-0 bg-black bg-opacity-70"></div>
         <div className="relative z-10 container mx-auto text-center">
-          <h1 className="text-6xl font-extrabold mb-6 leading-tight">
+          <h1 className="text-5xl md:text-6xl font-extrabold mb-6 leading-tight">
             Welcome Back, {user?.name || "Creative User"}!
           </h1>
-          <p className="text-lg mb-12">
-            Explore your design projects and elevate your creative journey with
-            cutting-edge tools.
+          <p className="text-lg mb-8 md:mb-12">
+            Explore your projects and enhance your creative journey.
           </p>
-          <button className="bg-teal-500 text-white py-3 px-6 rounded-full font-medium shadow-lg hover:bg-teal-600 transition duration-300">
+          <button className="bg-gradient-to-r from-teal-500 to-green-400 text-white py-3 px-8 rounded-full font-medium shadow-md hover:shadow-lg hover:scale-105 transform transition duration-300">
             Get Started
           </button>
         </div>
       </section>
 
       {/* Projects Section */}
-      <section className="py-16 bg-white">
+      <section className="py-16 bg-gray-100">
         <div className="container mx-auto px-6 text-center">
-          <h2 className="text-4xl font-bold text-gray-900 mb-16">
+          <h2 className="text-4xl font-bold text-gray-900 mb-12">
             Your Creative Projects
           </h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
             {projects.map((project) => (
               <div
                 key={project.idProject}
-                className="relative bg-white rounded-xl shadow-xl overflow-hidden transform hover:scale-105 transition-transform duration-300"
+                className="relative bg-white rounded-xl shadow-lg overflow-hidden hover:shadow-xl transform hover:-translate-y-2 transition-all duration-300"
               >
                 <img
                   src={project.image}
                   alt={project.name}
-                  className="w-full h-56 object-cover"
+                  className="w-full h-60 object-cover"
                 />
                 <div className="p-6">
-                  <h3 className="text-2xl font-semibold text-gray-800 mb-2">
+                  <h3 className="text-2xl font-semibold text-gray-800 mb-4">
                     {project.name}
                   </h3>
                   <p className="text-sm text-gray-500 mb-6">
                     {project.description || "No description available"}
                   </p>
-                  <div className="flex justify-center space-x-4">
+                  <div className="flex justify-between items-center space-x-4">
                     <button
                       onClick={() => handleEdit(project.idProject)}
-                      className="bg-teal-500 text-white py-2 px-4 rounded-full flex items-center shadow-lg hover:bg-teal-600 transition-colors duration-300"
+                      className="bg-teal-500 text-white py-2 px-4 rounded-full flex items-center shadow-md hover:bg-teal-600 transition-colors duration-300"
                     >
                       Edit
                       <RiEdit2Line className="ml-2" size={20} />
                     </button>
                     <button
                       onClick={() => handleDelete(project.idProject)}
-                      className="bg-red-500 text-white py-2 px-4 rounded-full flex items-center shadow-lg hover:bg-red-600 transition-colors duration-300"
+                      className="bg-red-500 text-white py-2 px-4 rounded-full flex items-center shadow-md hover:bg-red-600 transition-colors duration-300"
                     >
                       Delete
                       <RiDeleteBinLine className="ml-2" size={20} />
